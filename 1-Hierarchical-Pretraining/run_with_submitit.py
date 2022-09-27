4837dc415ce256f72a0877773b5fe5acc4ca0cbf
afilt <alexandre.filiot@owkin.com>
2022-09-27 15:09:45 +0000

    Slight adjustments to make code run.

--- 1-Hierarchical-Pretraining/run_with_submitit.py
+++ 1-Hierarchical-Pretraining/run_with_submitit.py
@@ -1,11 +1,11 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,21 +25,45 @@ import submitit
 
 
 def parse_args():
-    parser = argparse.ArgumentParser("Submitit for DINO", parents=[main_dino.get_args_parser()])
-    parser.add_argument("--ngpus", default=8, type=int, help="Number of gpus to request on each node")
-    parser.add_argument("--nodes", default=2, type=int, help="Number of nodes to request")
+    parser = argparse.ArgumentParser(
+        "Submitit for DINO", parents=[main_dino.get_args_parser()]
+    )
+    parser.add_argument(
+        "--ngpus", default=8, type=int, help="Number of gpus to request on each node"
+    )
+    parser.add_argument(
+        "--nodes", default=2, type=int, help="Number of nodes to request"
+    )
     parser.add_argument("--timeout", default=2800, type=int, help="Duration of the job")
 
-    parser.add_argument("--partition", default="learnfair", type=str, help="Partition where to submit")
-    parser.add_argument("--use_volta32", action='store_true', help="Big models? Use this")
-    parser.add_argument('--comment', default="", type=str,
-                        help='Comment to pass to scheduler, e.g. priority message')
+    parser.add_argument(
+        "--partition", default="gpu_p13", type=str, help="Partition where to submit"
+    )
+    parser.add_argument(
+        "--constraint", default=1200, type=int, help="Duration of the job (min)"
+    )
+    parser.add_argument(
+        "--comment",
+        default="",
+        type=str,
+        help="Comment to pass to scheduler, e.g. priority message",
+    )
     return parser.parse_args()
+
+
+# IBOT Charlie
+# def get_shared_folder() -> Path:
+#     user = os.getenv("USER")
+#     if Path("/gpfsscratch/rech/htc/uxu85mw/ibot/checkpoint/").is_dir():
+#         p = Path(f"/gpfsscratch/rech/htc/uxu85mw/ibot/checkpoint/{user}/experiments")
+#         p.mkdir(exist_ok=True)
+#         return p
+#     raise RuntimeError("No shared folder available")
 
 
 def get_shared_folder() -> Path:
     user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
+    if Path("/checkpoint/").is_dir():  # TODO: change
         p = Path(f"/checkpoint/{user}/experiments")
         p.mkdir(exist_ok=True)
         return p
@@ -79,7 +103,9 @@ class Trainer(object):
         from pathlib import Path
 
         job_env = submitit.JobEnvironment()
-        self.args.output_dir = Path(str(self.args.output_dir).replace("%j", str(job_env.job_id)))
+        self.args.output_dir = Path(
+            str(self.args.output_dir).replace("%j", str(job_env.job_id))
+        )
         self.args.gpu = job_env.local_rank
         self.args.rank = job_env.global_rank
         self.args.world_size = job_env.num_tasks
@@ -100,9 +126,9 @@ def main():
     partition = args.partition
     kwargs = {}
     if args.use_volta32:
-        kwargs['slurm_constraint'] = 'volta32gb'
+        kwargs["slurm_constraint"] = "volta32gb"
     if args.comment:
-        kwargs['slurm_comment'] = args.comment
+        kwargs["slurm_comment"] = args.comment
 
     executor.update_parameters(
         mem_gb=40 * num_gpus_per_node,
@@ -114,7 +140,7 @@ def main():
         # Below are cluster dependent parameters
         slurm_partition=partition,
         slurm_signal_delay_s=120,
-        **kwargs
+        **kwargs,
     )
 
     executor.update_parameters(name="dino")
